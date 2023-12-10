8c68006373b56b8ba9f8a00f5f84bedc9a4577c4
Mehedi Hasan Shuvo <67133203+duetianmehedishuvo@users.noreply.github.com>
2023-12-11 01:23:50 +0600

    update Ph and Dissolved oxigen sensor calibration

--- dissolved_oxygen2.ino
+++ dissolved_oxygen2.ino
@@ -1,46 +1,18 @@
 #include <Arduino.h>
  
-#define DO_PIN A0
+#define DO_PIN 39
  
 #define VREF 5000    //VREF (mv)
 #define ADC_RES 1024 //ADC Resolution
- 
-//Single-point calibration Mode=0
-//Two-point calibration Mode=1
-#define TWO_POINT_CALIBRATION 0
- 
-#define READ_TEMP (25) //Current water temperature ℃, Or temperature sensor function
- 
-//Single point calibration needs to be filled CAL1_V and CAL1_T
-#define CAL1_V (131) //mv
-#define CAL1_T (25)   //℃
-//Two-point calibration needs to be filled CAL2_V and CAL2_T
-//CAL1 High temperature point, CAL2 Low temperature point
-#define CAL2_V (4296) //mv
-#define CAL2_T (30)   //℃
- 
-const uint16_t DO_Table[41] = {
-    14460, 14220, 13820, 13440, 13090, 12740, 12420, 12110, 11810, 11530,
-    11260, 11010, 10770, 10530, 10300, 10080, 9860, 9660, 9460, 9270,
-    9080, 8900, 8730, 8570, 8410, 8250, 8110, 7960, 7820, 7690,
-    7560, 7430, 7300, 7180, 7070, 6950, 6840, 6730, 6630, 6530, 6410};
  
 uint8_t Temperaturet;
 uint16_t ADC_Raw;
 uint16_t ADC_Voltage;
 uint16_t DO;
+float DO2;
 
 float dhValue;
-int16_t readDO(uint32_t voltage_mv, uint8_t temperature_c)
-{
-#if TWO_POINT_CALIBRATION == 00
-  uint16_t V_saturation = (uint32_t)CAL1_V + (uint32_t)35 * temperature_c - (uint32_t)CAL1_T * 35;
-  return (voltage_mv * DO_Table[temperature_c] / V_saturation);
-#else
-  uint16_t V_saturation = (int16_t)((int8_t)temperature_c - CAL2_T) * ((uint16_t)CAL1_V - CAL2_V) / ((uint8_t)CAL1_T - CAL2_T) + CAL2_V;
-  return (voltage_mv * DO_Table[temperature_c] / V_saturation);
-#endif
-}
+
  
 void setup()
 {
@@ -57,10 +29,26 @@ void loop()
   if(ADC_Voltage!=0){
     dhValue=abs(readDO(ADC_Voltage, Temperaturet)/100.0);
   }
+
+  if(ADC_Voltage<=2000){
+    DO2=ADC_Voltage*(1-0.99575172);
+  }else if(ADC_Voltage>2000 && ADC_Voltage<=4000){
+    DO2=ADC_Voltage*(1-0.997511078);
+  }else if(ADC_Voltage>4000 && ADC_Voltage<=6000){
+    DO2=ADC_Voltage*(1-0.998152371);
+  }else if(ADC_Voltage>6000 && ADC_Voltage<=10000){
+    DO2=ADC_Voltage*(1-0.9972315618);
+  }else{
+    DO2=ADC_Voltage*(1-0.9991858652);
+  }
+  
+  
+  
  
-  Serial.print("Temperaturet:\t" + String(Temperaturet) + "\t");
-  Serial.print("ADC RAW:\t" + String(ADC_Raw) + "\t");
+//  Serial.print("Temperaturet:\t" + String(Temperaturet) + "\t");
+//  Serial.print("ADC RAW:\t" + String(ADC_Raw) + "\t");
   Serial.print("ADC Voltage:\t" + String(ADC_Voltage) + "\t");
+  Serial.print("DO 2:\t" + String(DO2) + "\t");
   Serial.print("DO:\t" + String(readDO(ADC_Voltage, Temperaturet)) + "\t");
   Serial.println("DO Voltage:\t" + String(dhValue) + "\t");
  
